c93b17aa0be6243a8bc8f1b67d28b9876a8b3bc8
rl1987 <rl1987@users.noreply.github.com>
2022-06-16 12:54:31 +0300

    Add TF config with provisioning script

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = "1c6508e264c30fcc6249f937d2d4eb2cacd6ebbf25b1b7cd300b777cb2339195"
+}
+
+resource "digitalocean_droplet" "server" {
+  image     = "debian-11-x64"
+  name      = "dhdb-museum-collections"
+  region    = "sfo3"
+  size      = "s-1vcpu-1gb"
+  ssh_keys  = ["50:ba:8f:a6:1a:e5:82:f8:57:5b:a0:c5:6e:00:f6:99"]
+  user_data = file("provision.sh")
+
+  connection {
+    host        = self.ipv4_address
+    user        = "root"
+    type        = "ssh"
+    timeout     = "2m"
+    private_key = file("~/.ssh/id_rsa")
+  }
+
+  provisioner "file" {
+    source      = "~/.dolt"
+    destination = "/root"
+  }
+}
+
+output "server_ip" {
+  value = resource.digitalocean_droplet.server.ipv4_address
+}
+
---
+++ provision.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+
+apt-get update
+apt-get install -y python3 python3-pip tmux git vim visidata
+
+curl -L https://github.com/dolthub/dolt/releases/latest/download/install.sh > /tmp/install.sh && bash /tmp/install.sh
+dolt config --global --add user.email rimantas@keyspace.lt
+dolt config --global --add user.name "rl1987"
+
+pip3 install --upgrade requests lxml js2xml doltpy scrapy
+
+curl -sSL https://repos.insights.digitalocean.com/install.sh -o /tmp/install.sh
+bash /tmp/install.sh
+
+mkdir /root/data
+
+pushd /root/data
+dolt clone rl1987/museum-collections
+popd /root/data
