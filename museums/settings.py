16064b6c73be9a885dcdf32edc5d0f1c211ac178
rl1987 <rl1987@users.noreply.github.com>
2022-06-16 17:43:40 +0300

    Disregard robots.txt rules

--- museums/settings.py
+++ museums/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'museums.spiders'
 #USER_AGENT = 'museums (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
