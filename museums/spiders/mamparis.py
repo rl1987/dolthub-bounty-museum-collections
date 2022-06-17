d5489489bf3f9ed2ec319053c3d236fc8bb53203
rl1987 <rl1987@users.noreply.github.com>
2022-06-17 16:24:04 +0300

    Implement API page traversal

--- museums/spiders/mamparis.py
+++ museums/spiders/mamparis.py
@@ -1,10 +1,44 @@
 import scrapy
 
+from urllib.parse import urlparse, parse_qsl, urlencode
+import json
 
 class MamparisSpider(scrapy.Spider):
     name = 'mamparis'
-    allowed_domains = ['mam.paris.fr']
+    allowed_domains = ['mam.paris.fr', 'api.navigart.fr']
     start_urls = ['http://mam.paris.fr/']
 
-    def parse(self, response):
-        pass
+    def start_requests(self):
+        params = {
+            'sort': 'by_author',
+            'size': 60,
+            'from': 0
+        }
+
+        url = 'https://api.navigart.fr/18/artworks?' + urlencode(params)
+
+        yield scrapy.Request(url, callback=self.parse_search_results)
+    
+    def parse_search_results(self, response):
+        json_dict = json.loads(response.text)
+
+        results = json_dict.get("results")
+        if results is None or len(results) == 0:
+            return
+
+        # TODO: parse data into items
+
+        if len(results) < 60:
+            return
+
+        o = urlparse(response.url)
+        old_params = dict(parse_qsl(o.query))
+
+        new_params = dict(old_params)
+
+        new_params['from'] = int(new_params['from']) + 60
+
+        url = 'https://api.navigart.fr/18/artworks?' + urlencode(new_params)
+
+        yield scrapy.Request(url, callback=self.parse_search_results)
+
