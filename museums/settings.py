f06079c471fd60aa9b8aa389861e0c16bede73f8
rl1987 <rl1987@users.noreply.github.com>
2022-06-21 12:36:48 +0300

    Set User-Agent header

--- museums/settings.py
+++ museums/settings.py
@@ -12,9 +12,7 @@ BOT_NAME = 'museums'
 SPIDER_MODULES = ['museums.spiders']
 NEWSPIDER_MODULE = 'museums.spiders'
 
-
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'museums (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
