14dd3480a2d2a2bfb343f520e75d3e3437b3d659
rl1987 <rl1987@users.noreply.github.com>
2022-06-21 14:10:55 +0300

    Scrape some of the fields from object API

--- museums/spiders/britishmuseum.py
+++ museums/spiders/britishmuseum.py
@@ -1,7 +1,10 @@
 import scrapy
+from scrapy.selector import Selector
 
 import json
 from urllib.parse import urlparse, parse_qsl, urlencode
+
+from museums.items import ObjectItem
 
 class BritishmuseumSpider(scrapy.Spider):
     name = 'britishmuseum'
@@ -44,6 +47,56 @@ class BritishmuseumSpider(scrapy.Spider):
                 callback=self.parse_search_api_response)
 
     def parse_object_api_response(self, response):
-        pass
+        json_dict = json.loads(response.text)
+        source_dict = json_dict.get("hits", dict()).get("hits")[0].get("_source")
 
+        o = urlparse(response.url)
 
+        params = dict(parse_qsl(o.query))
+
+        item = ObjectItem()
+
+        item['object_number'] = params.get("id")
+        
+        item['institution_name'] = 'British Museum'
+        item['institution_city'] = 'London'
+        item['institution_country'] = 'United Kingdom'
+        item['institution_latitude'] = 51.518757
+        item['institution_longitude'] = -0.126168
+        
+        # XXX: category
+
+        xtemplate_full_json_str = source_dict.get("xtemplate", dict()).get("full")
+        xtemplate_full_json_dict = json.loads(xtemplate_full_json_str)
+
+        if type(xtemplate_full_json_dict.get("Title")) == list:
+            title_parts = []
+
+            for title_html in xtemplate_full_json_dict.get("Title", []):
+                sel = Selector(text=title_html)
+                title_part = " ".join(sel.xpath("//text()").getall())
+                title_parts.append(title_part)
+
+            item['title'] = " ".join(title_parts)
+        elif type(xtemplate_full_json_dict.get("Title")) == str:
+            title_html = xtemplate_full_json_dict.get("Title")
+            sel = Selector(text=title_html)
+            item['title'] = " ".join(sel.xpath("//text()").getall())
+
+        item['department'] = xtemplate_full_json_dict.get("Department")
+        item['description'] = " ".join(xtemplate_full_json_dict.get("Description", []))
+        item['current_location'] = xtemplate_full_json_dict.get("Location")
+        
+        dimensions = []
+
+        for dimension_html in xtemplate_full_json_dict.get("Dimensions", []):
+            sel = Selector(text=dimension_html)
+            dimension = " ".join(sel.xpath("//text()").getall())
+            dimensions.append(dimension)
+
+        item['dimensions'] = "|".join(dimensions)
+
+        item['source_1'] = 'https://www.britishmuseum.org/collection/object/' + params.get("id")
+        item['source_2'] = response.url
+
+        yield item
