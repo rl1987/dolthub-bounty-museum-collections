702a36469689ff2999213a8e6115a29c715534f9
rl1987 <rl1987@users.noreply.github.com>
2022-06-21 17:55:35 +0300

    Add new Python file for trying to scrape Victorian govt. API

---
+++ mvc_api_scraping/mvc_api_scraping.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import csv
+import time
+
+import requests
+
+# https://www.developer.vic.gov.au/index.php?option=com_apiportal&view=apitester&usage=api&tab=tests&apiName=Museums+Victoria+Collections+API&apiId=e41d370d-d729-4ae2-9eb2-0cfb804ebad5&managerId=1&type=rest&apiVersion=1.0.0&menuId=153&renderTool=1#!/collections/getSpecies
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
