1bd4fa9bf24d722804b1f516293797cc8079d8ef
rl1987 <rl1987@users.noreply.github.com>
2022-06-28 19:13:35 +0300

    Traverse further pages on eHive

--- museums/spiders/ehive.py
+++ museums/spiders/ehive.py
@@ -21,16 +21,38 @@ class EhiveSpider(scrapy.Spider):
             yield scrapy.Request(next_page_url, callback=self.parse_collection_list)
 
     def parse_museum_page(self, response):
-        institution_name = response.xpath('//span[@id="publicProfileName"]/text()').get()
+        institution_name = response.xpath("//title/text()").get().replace(" on eHive", "")
         institution_latitude = response.xpath('//span[@id="latitude"]/text()').get()
         institution_longitude = response.xpath('//span[@id="longitude"]/text()').get()
 
+        meta = {
+            'institution_name': institution_name,
+            'institution_latitude': institution_latitude,
+            'institution_longitude': institution_longitude
+        }
+                
+
+        # TODO: come up with a way to remove data from what appears to be test profiles
         view_all_link = response.xpath('//h5[text()="Entire collection - "]/a/@href').get()
         view_all_url = urljoin(response.url, view_all_link)
-        yield scrapy.Request(view_all_url, callback=self.parse_object_list)
+        yield scrapy.Request(view_all_url, callback=self.parse_object_list, meta=meta)
 
     def parse_object_list(self, response):
-        pass
+        meta = {
+            'institution_name': response.meta.get('institution_name'),
+            'institution_latitude': response.meta.get('institution_latitude'),
+            'institution_longitude': response.meta.get('institution_longitude')
+        }
+
+        for object_link in response.xpath('//a[./img[@class="eh-image"]]/@href').getall():
+            object_url = urljoin(response.url, object_link)
+            yield scrapy.Request(object_url, callback=self.parse_object_page, meta=meta)
+
+        next_page_link = response.xpath('//a[@aria-label="Next"]/@href').get()
+        if next_page_link is not None:
+            next_page_url = urljoin(response.url, next_page_link)
+            yield scrapy.Request(next_page_url, callback=self.parse_object_list, meta=meta)
 
     def parse_object_page(self, response):
-        pass
+        self.logger.debug(response.meta)
+
