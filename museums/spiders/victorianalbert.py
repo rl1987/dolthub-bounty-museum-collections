a928b51b07a545440482b9fcc0ce90f0f3f1b85d
rl1987 <rl1987@users.noreply.github.com>
2022-07-01 20:44:26 +0300

    Extract data from object pages

--- museums/spiders/victorianalbert.py
+++ museums/spiders/victorianalbert.py
@@ -3,6 +3,8 @@ import scrapy
 import json
 from urllib.parse import urlparse, urlencode, parse_qsl
 
+from museums.items import ObjectItem
+
 # https://collections.vam.ac.uk/search/?page=1&page_size=15
 
 class VictoriaNAlbertSpider(scrapy.Spider):
@@ -66,7 +68,9 @@ class VictoriaNAlbertSpider(scrapy.Spider):
         old_params = dict(parse_qsl(o.query))
 
         records = json_dict.get("records", [])
-        
+        for record in records:
+            url = 'https://collections.vam.ac.uk/item/' + record.get("systemNumber")
+            yield scrapy.Request(url, callback=self.parse_object_page)
 
         if len(records) < int(old_params.get('page_size')):
             return
@@ -78,5 +82,40 @@ class VictoriaNAlbertSpider(scrapy.Spider):
         yield scrapy.Request(url, callback=self.parse_search_page)
 
     def parse_object_page(self, response):
-        pass
+        item = ObjectItem()
+    
+        item['object_number'] = response.xpath('//meta[@property="og:url"]/@content').get().replace('/item/', '').replace('/', '')
+        item['institution_name'] = 'Victoria and Albert Museum'
+        item['institution_city'] = 'London'
+        item['institution_state'] = 'England'
+        item['institution_country'] = 'United Kingdom'
+        item['institution_latitude'] = 51.494720458984375
+        item['institution_longitude'] = -0.1917800009250641
+        item['category'] = response.xpath('//a[@data-tracking-collections="tag - category"]/div/text()').get()
+        item['title'] = response.xpath('//h1[@class="object-page__title"]/text()').get()
+        # XXX: current_location
+        item['dimensions'] = "|".join(response.xpath('//tr[./td[text()="Dimensions"]]//li/text()').getall())
+        item['inscription'] = "|".join(response.xpath('//tr[./td[text()="Marks and Inscriptions"]]//li/text()').getall())
+        item['provenance'] = response.xpath('//tr[./td[text()="Object history"]]//div[@class="etc-details__cell-free-content"]/text()').get()
+        item['technique'] = "|".join(response.xpath('//a[@data-tracking-collections="tag - material"]/div/text()').getall())
+        item['from_location'] = "|".join(response.xpath('//a[@data-tracking-collections="tag - place"]/div/text()').getall())
+        # XXX: date_description, year_start, year end
+
+        makers = response.xpath('//tr[./td[text()="Artist/Maker"]]//div[@class="etc-details__controlled-vocab-content"]/text()').getall()
+
+        maker_names = []
+        maker_roles = []
+
+        for m in makers:
+            maker_names.append(m.split(" (")[0])
+            maker_roles.append(m.split(" (")[-1].replace(")", ""))
+
+        item['maker_full_name'] = "|".join(maker_names)
+        item['maker_role'] = "|".join(maker_roles)
+        item['accession_number'] = response.xpath('//tr[./td[text()="Accession Number"]]//div/text()').get()
+        item['credit_line'] = response.xpath('//tr[./td[text()="Credit line"]]//div/text()').get()
+        item['image_url'] = response.xpath('//img[@class="object-page__hero-img"]/@src').get()
+        item['source_1'] = response.url
+
+        yield item
 
