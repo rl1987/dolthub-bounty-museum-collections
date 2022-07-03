8bcb467c9484f5b0c634a023454947f82caffeb5
rl1987 <rl1987@users.noreply.github.com>
2022-07-03 17:40:45 +0300

    Scrape more tables from Wikipedia

--- recon/wiki.py
+++ recon/wiki.py
@@ -13,6 +13,21 @@ def main():
     print(df)
     df.to_csv("wiki_most_visited_art_museums.csv")
 
+    dfs = pd.read_html("https://en.wikipedia.org/wiki/List_of_most-visited_museums_in_the_United_States")
+    df = dfs[0]
+    print(df)
+    df.to_csv("wiki_usa_most_visited_museums.csv")
+
+    dfs = pd.read_html("https://en.wikipedia.org/wiki/List_of_most_visited_museums_in_the_United_Kingdom")
+    df = dfs[0]
+    print(df)
+    df.to_csv("wiki_uk_most_visited_museums.csv")
+
+    dfs = pd.read_html("https://en.wikipedia.org/wiki/List_of_most-visited_museums")
+    df = dfs[0]
+    print(df)
+    df.to_csv("wiki_global_most_visited_museums.csv")
+
 if __name__ == "__main__":
     main()
 
