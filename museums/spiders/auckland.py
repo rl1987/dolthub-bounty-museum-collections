386e400e57a51b00d3c8a496e0c4ee2cd317eaea
rl1987 <rl1987@users.noreply.github.com>
2022-07-07 15:51:02 +0300

    Try sharding search to bypass 10000 result limit

--- museums/spiders/auckland.py
+++ museums/spiders/auckland.py
@@ -1,10 +1,36 @@
 import scrapy
 
+import string
+from urllib.parse import urlencode
 
 class AucklandSpider(scrapy.Spider):
     name = 'auckland'
     allowed_domains = ['aucklandmuseum.com']
-    start_urls = ['http://aucklandmuseum.com/']
+    start_urls = ['https://www.aucklandmuseum.com/discover/collections-online/search']
+    
+    def start_requests(self):
+        yield scrapy.Request(self.start_urls[0])
 
     def parse(self, response):
+        categories = ["ecrm:E22_Man-Made_Object", "ecrm:E84_Information_Carrier", "ecrm:E20_Biological_Object", "am_isTaonga"]
+        departments = ["Applied Arts and Design", "Archaeology", "Botany", "Entomology", "Ethnology", "Geology", "History", "Maori",
+                "Marine", "Māori", "Pacific", "Pictorial", "World", "[library]", "birds", "botany", "ephemera", "land mammals",
+                "manuscripts and archives", "painting and drawings", "photography", "publication", "reptiles and amphibians"]
+
+        search_url = "https://www.aucklandmuseum.com/discover/collections-online/search"
+        
+        for letter in string.ascii_lowercase:
+            for c in categories:
+                for d in departments:
+                    params = {
+                        'c': c,
+                        'dept': d,
+                        'k': letter
+                    }
+
+                    url = search_url + "?" + urlencode(params)
+
+                    yield scrapy.Request(url, callback=self.parse_search_page)
+
+    def parse_search_page(self, response):
         pass
