917cf6f97b34689dd9f8d841c78111d7d36a319f
rl1987 <rl1987@users.noreply.github.com>
2022-07-08 17:08:07 +0300

    Extract object information from object pages

--- museums/spiders/iwm.py
+++ museums/spiders/iwm.py
@@ -2,6 +2,8 @@ import scrapy
 
 from urllib.parse import urljoin
 
+from museums.items import ObjectItem
+
 class IwmSpider(scrapy.Spider):
     name = 'iwm'
     allowed_domains = ['iwm.org.uk']
@@ -22,4 +24,25 @@ class IwmSpider(scrapy.Spider):
             yield scrapy.Request(next_page_url, callback=self.parse_search_page)
 
     def parse_object_page(self, response):
-        pass
+        item = ObjectItem()
+        
+        item['object_number'] = response.url.split("/")[-1]
+        item['institution_name'] = 'Imperial War Museum'
+        item['institution_city'] = 'London'
+        item['institution_state'] = 'England'
+        item['institution_country'] = 'United Kingdom'
+        item['institution_latitude'] = 51.4981985
+        item['institution_longitude'] = -0.1196327
+        item['category'] = response.xpath('//dt[text()="Category"]/following-sibling::dd/a/text()').get()
+        item['title'] = response.xpath('//h1[@class="hero__truncate"]/text()').get("").strip()
+        item['description'] = response.xpath('//div[@class="object-description__text"]/text()').get()
+        item['dimensions'] = "|".join(response.xpath('//dt[text()="Dimensions"]/following-sibling::dd/p/text()').getall())
+        item['materials'] = "|".join(response.xpath('//dt[text()="Materials"]/following-sibling::dd/p/text()').getall())
+        item['from_location'] = response.xpath('//dt[text()="Place made"]/following-sibling::dd/a/text()').get()
+        item['date_description'] = response.xpath('//dt[text()="Production date"]/following-sibling::dd/text()').get()
+        item['maker_full_name'] = "|".join(response.xpath('//dt[text()="Creator"]/following-sibling::dd/a/text()').getall())
+        item['accession_number'] = response.xpath('//dt[text()="Catalogue number"]/following-sibling::dd/text()').get()
+        item['image_url'] = response.xpath('//meta[@property="og:image"]/@content').get()
+        item['source_1'] = response.url
+
+        yield item
