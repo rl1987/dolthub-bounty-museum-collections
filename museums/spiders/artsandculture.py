af75bd08b6b346f1e46c618e9689f1c91d3bca5e
rl1987 <rl1987@users.noreply.github.com>
2022-07-10 13:46:52 +0300

    Start working on crawling the site. Prepare stubs for further code.

--- museums/spiders/artsandculture.py
+++ museums/spiders/artsandculture.py
@@ -1,10 +1,41 @@
 import scrapy
 
+import js2xml
+
+from urllib.parse import urljoin, urlencode, parse_qsl
 
 class ArtsAndCultureSpider(scrapy.Spider):
     name = 'artsandculture'
     allowed_domains = ['artsandculture.google.com']
-    start_urls = ['http://artsandculture.google.com/']
+    start_urls = ['https://artsandculture.google.com/search/partner']
 
-    def parse(self, response):
+    def start_requests(self):
+        yield scrapy.Request(self.start_urls[0], callback=self.parse_partners_html_page)
+
+    def parse_partners_html_page(self, response):
+        js = response.xpath('//script[contains(text(), "INIT_data")]/text()').get()
+        parsed = js2xml.parse(js)
+        links = parsed.xpath('//string[starts-with(text(), "/partner/")]/text()')
+
+        for l in links:
+            stub = stub = l.split('/')[-1]
+
+            params = {
+                'p': stub
+            }
+
+            url = 'https://artsandculture.google.com/search/asset/?' + urlencode(params)
+            yield scrapy.Request(url, callback=self.parse_asset_search_html_page)
+
+    def parse_partners_api_response(self, response):
         pass
+
+    def parse_asset_search_html_page(self, response):
+        pass
+
+    def parse_asset_search_api_response(self, response):
+        pass
+
+    def parse_asset_html_page(self, response):
+        pass
+
