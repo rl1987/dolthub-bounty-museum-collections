aebdefc2caa82e2bfded06ff88b832cca719ec76
rl1987 <rl1987@users.noreply.github.com>
2022-07-13 13:56:21 +0300

    Extract fields from object pages on Science Museum Group site

--- museums/spiders/science.py
+++ museums/spiders/science.py
@@ -2,6 +2,8 @@ import scrapy
 
 from urllib.parse import urlparse, urlencode, parse_qsl
 
+from museums.items import ObjectItem
+
 class ScienceSpider(scrapy.Spider):
     name = 'science'
     allowed_domains = ['collection.sciencemuseumgroup.org.uk']
@@ -40,4 +42,31 @@ class ScienceSpider(scrapy.Spider):
             yield scrapy.Request(next_page_url, callback=self.parse_search_page)
 
     def parse_object_page(self, response):
-        pass
+        item = ObjectItem()
+
+        item['object_number'] = response.xpath('//dl[contains(@class, "details-Object-Number") or contains(@class, "details-Identifier")]/dd/text()').get("").strip()
+        item['institution_name'] = "Science Museum Group"
+        item['institution_country'] = "United Kingdom"
+        item['department'] = response.xpath('//dl[contains(@class, "details-Collection")]/dd/a/text()').get("").strip()
+        item['category'] = response.xpath('//dl[contains(@class, "details-Category")]/dd/a/text()').get("").strip()
+        item['title'] = response.xpath('//h1[@class="record-top__title"]/text()').get("").strip()
+        item['description'] = response.xpath('//meta[@property="og:description"]/@content').get()
+        item['current_location'] = response.xpath('//div[./h2[contains(text(), "On display")]]/p/a/text()').get()
+        item['dimensions'] = " ".join(response.xpath('//dl[contains(@class, "details-Measurements")]/dd/text()').getall()).strip()
+        materials = response.xpath('//dl[contains(@class, "details-Materials")]/dd/a/text()').getall()
+        materials = list(map(lambda m: m.strip(), materials))
+        materials = "|".join(materials)
+        item['materials'] = materials
+        item['from_location'] = response.xpath('//dl[contains(@class, "fact-Made")]/dd/a[2]/text()').get("").strip()
+        item['date_description'] = response.xpath('//dl[contains(@class, "fact-Made")]/dd/a[1]/text()').get("").strip()
+        makers = response.xpath('//dl[contains(@class, "fact-maker")]/dd/a/text()').getall()
+        makers = list(map(lambda m: m.strip(), makers))
+        makers = "|".join(makers)
+        item['maker_full_name'] = makers
+        item['credit_line'] = response.xpath('//dl[contains(@class, "details-credit")]/dd/text()').get("").strip()
+
+        item['image_url'] = response.xpath('//div[@class="carousel"]/img/@data-flickity-lazyload').get()
+        item['source_1'] = response.url
+        item['source_2'] = response.xpath('//a[./abbr[@title="Javascript Object Notation"]]/@href').get()
+
+        yield item
