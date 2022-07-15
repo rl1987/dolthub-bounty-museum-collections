79f5864ed0989b2e1f02b88354ca534a35b11040
rl1987 <rl1987@users.noreply.github.com>
2022-07-15 19:21:01 +0300

    Extract item fields from Ashmolean Museum pages

--- museums/spiders/ashmolean.py
+++ museums/spiders/ashmolean.py
@@ -1,5 +1,6 @@
 import scrapy
 
+from museums.items import ObjectItem
 
 class AshmoleanSpider(scrapy.Spider):
     name = 'ashmolean'
@@ -18,4 +19,29 @@ class AshmoleanSpider(scrapy.Spider):
             yield scrapy.Request(next_page_url, callback=self.parse_search_page)
 
     def parse_object_page(self, response):
-        pass
+        item = ObjectItem()
+
+        item['source_1'] = response.xpath('//input[@id="ref-url"]/@value').get()
+
+        item['object_number'] = item['source_1'].split('/')[-1]
+        item['institution_name'] = 'Ashmolean Museum'
+        item['institution_city'] = 'Oxford'
+        item['institution_state'] = 'England'
+        item['institution_country'] = 'United Kingdom'
+        item['institution_latitude'] = 51.755332
+        item['institution_longitude'] = -1.2611322
+        item['department'] = response.xpath('//li[./div/p[text()="Museum department"]]/div[@class="right-wrap"]//p/text()').get("").strip()
+        item['category'] = response.xpath('//li[./div/p[text()="Object type"]]/div[@class="right-wrap"]//a/text()').get("").strip()
+        item['title'] = " ".join(response.xpath('//h1/text()').getall())
+        item['description'] = " ".join(response.xpath('//div[@class="item-descrption"]/div/p/text()').getall())
+        item['current_location'] = response.xpath('//li[./div/p[text()="Museum location"]]/div[@class="right-wrap"]//a/text()').get("").strip()
+        item['dimensions'] = response.xpath('//li[./div/p[text()="Dimensions"]]/div[@class="right-wrap"]/div/text()').get("").strip()
+        item['materials'] = response.xpath('//li[./div/p[text()="Material and technique"]]/div[@class="right-wrap"]/div/p/text()').get("").strip()
+        item['from_location'] = "|".join(response.xpath('//li[./div/p[text()="Associated place"]]/div[@class="right-wrap"]//a/text()').getall())
+        item['date_description'] = response.xpath('//li[./div/p[text()="Date"]]/div[@class="right-wrap"]/text()').get("").strip()
+        item['maker_full_name'] = response.xpath('//li[./div/p[text()="Artist/maker"]]/div[@class="right-wrap"]//a/text()').get("").strip()
+        item['accession_number'] = response.xpath('//li[./div/p[text()="Accession no."]]/div[@class="right-wrap"]/div/p/text()').get("").strip()
+        item['credit_line'] = response.xpath('//li[./div/p[text()="Credit line"]]/div[@class="right-wrap"]/div/p/text()').get("").strip()
+        item['image_url'] = response.xpath('//div[@class="firstimg"]/a/@href').get()
+
+        yield item
