f08ea15488b67c68ab8e0020b63eb81a35729c27
rl1987 <rl1987@users.noreply.github.com>
2022-07-15 18:36:04 +0300

    Crawl the pages on Ashmolean Museum site

--- museums/spiders/ashmolean.py
+++ museums/spiders/ashmolean.py
@@ -6,5 +6,16 @@ class AshmoleanSpider(scrapy.Spider):
     allowed_domains = ['collections.ashmolean.org']
     start_urls = ['https://collections.ashmolean.org/collection/browse-9148']
 
-    def parse(self, response):
+    def start_requests(self):
+        yield scrapy.Request(self.start_urls[0], callback=self.parse_search_page)
+
+    def parse_search_page(self, response):
+        for url in response.xpath('//ul[@class="vr-list"]/li/a/@href').getall():
+            yield scrapy.Request(url, callback=self.parse_object_page)
+
+        next_page_url = response.xpath('//a[@class="next-btn"]/@href').get()
+        if next_page_url is not None:
+            yield scrapy.Request(next_page_url, callback=self.parse_search_page)
+
+    def parse_object_page(self, response):
         pass
