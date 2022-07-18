e4024a78b8304fef18f2e85936fe91d6799cc9c4
rl1987 <rl1987@users.noreply.github.com>
2022-07-18 17:41:12 +0300

    Simplify MucemSpider to iterate across item indices

--- museums/spiders/mucem.py
+++ museums/spiders/mucem.py
@@ -9,30 +9,13 @@ from museums.items import ObjectItem
 class MucemSpider(scrapy.Spider):
     name = 'mucem'
     allowed_domains = ['www.mucem.org']
-    start_urls = ['https://www.mucem.org/en/collections/explorez-les-collections/next-page?term=a&page=1']
 
     def start_requests(self):
-        yield scrapy.Request(self.start_urls[0], callback=self.parse_search_page)
+        for i in range(1, 500000):
+            data_url = "http://data.mucem.org/c/{}".format(i)
+            url = "https://www.mucem.org/en/collections/explorez-les-collections/objet?uri=" + data_url
 
-    def parse_search_page(self, response):
-        json_str = response.text
-        json_dict = json.loads(json_str)
-        html_str = json_dict.get("html")
-
-        sel = Selector(text=html_str)
-
-        for url in sel.xpath('//a/@href').getall():
             yield scrapy.Request(url, callback=self.parse_object_page)
-
-        is_next_page = json_dict.get("next_button", False)
-        if is_next_page:
-            o = urlparse(response.url)
-            params = dict(parse_qsl(o.query))
-            params['page'] = int(params['page']) + 1
-
-            next_page_url = 'https://www.mucem.org/en/collections/explorez-les-collections/next-page?' + urlencode(params)
-
-            yield scrapy.Request(next_page_url, callback=self.parse_search_page)
 
     def parse_object_page(self, response):
         item = ObjectItem()
