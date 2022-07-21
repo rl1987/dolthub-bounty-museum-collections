749a7084b93b3049d2a35e5cc340a4a6ab86a0b9
rl1987 <rl1987@users.noreply.github.com>
2022-07-21 13:17:21 +0300

    Traverse the pages on National Portrait Gallery of London

--- museums/spiders/npglondon.py
+++ museums/spiders/npglondon.py
@@ -1,10 +1,24 @@
 import scrapy
 
+import string
 
 class NpglondonSpider(scrapy.Spider):
     name = 'npglondon'
     allowed_domains = ['www.npg.org.uk']
-    start_urls = ['http://www.npg.org.uk/']
+    start_urls = [ "https://www.npg.org.uk/collections/search/arta-z/?index=" + letter for letter in string.ascii_lowercase ]
 
-    def parse(self, response):
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url, callback=self.parse_artist_list)
+
+    def parse_artist_list(self, response):
+        for l in response.xpath('//div[@id="eventsListing"]/p/a/@href').getall():
+            yield response.follow(l, callback=self.parse_portrait_list)
+
+    def parse_portrait_list(self, response):
+        for l in response.xpath('//p[@class="eventHeading"]/a/@href').getall():
+            yield response.follow(l, callback=self.parse_portrait_page)
+
+    def parse_portrait_page(self, response):
         pass
+
