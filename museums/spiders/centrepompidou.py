bd6f0ad8a7b14a36263be43a6498c49ab0837d5a
rl1987 <rl1987@users.noreply.github.com>
2022-07-21 15:54:01 +0300

    Extract item data from Centre Pompidou

--- museums/spiders/centrepompidou.py
+++ museums/spiders/centrepompidou.py
@@ -7,6 +7,7 @@ from scrapy.selector import Selector
 from scrapy.http import FormRequest
 
 from urllib.parse import urlencode, urlparse
+from museums.items import ObjectItem
 
 class CentrepompidouSpider(scrapy.Spider):
     name = 'centrepompidou'
@@ -54,5 +55,30 @@ class CentrepompidouSpider(scrapy.Spider):
         yield FormRequest(response.url, formdata=form_data, callback=self.parse_search_page, meta={'formdata': form_data})
 
     def parse_object_page(self, response):
-        pass
+        item = ObjectItem()
+
+        item['object_number'] = response.xpath('//tr[./th[contains(text(), "Inventory no.")]]/td/text()').get("").strip()
+        item['institution_name'] = 'Centre Pompidou'
+        item['institution_city'] = 'Paris'
+        item['institution_state'] = 'Île-de-France'
+        item['institution_country'] = 'France'
+        item['institution_latitude'] = 48.8608975
+        item['institution_longitude'] = 2.3495319
+        item['department'] = response.xpath('//tr[./th[contains(text(), "Collection area")]]/td/p/text()').get()
+        item['category'] = "|".join(response.xpath('//table[@class="table"]//tr[./th[contains(text(), "Domain")]]/td/a/text()').getall())
+        item['title'] = response.xpath('//h1/text()').get("").strip()
+        item['dimensions'] =  response.xpath('//tr[./th[contains(text(), "Dimensions")]]/td/p/text()').get()
+        item['inscription'] = " ".join(response.xpath('//tr[./th[contains(text(), "Inscriptions")]]/td/p/text()').getall())
+        item['provenance'] = response.xpath('//tr[./th[contains(text(), "Notes")]]/td/p/text()').get()
+        item['technique'] = response.xpath('//tr[./th[contains(text(), "Techniques")]]/td/p/text()').get()
+        item['date_description'] = response.xpath('//p[@class="concept-date"]/text()').get()
+        maker_names = response.xpath('//p[@class="concept-authors"]/a/text()').getall()
+        maker_names = list(map(lambda m: m.strip(), maker_names))
+        maker_names = "|".join(maker_names)
+        item['maker_full_name'] = maker_names
+        item['acquired_from'] = response.xpath('//tr[./th[contains(text(), "Acquisition")]]/td/p/text()').get()
+        item['image_url'] = response.xpath('//meta[@property="og:image"]/@content').get()
+        item['source_1'] = response.url
+
+        yield item
 
