ecb8bb7a1272de7930d3f5d5759f570d067fda77
rl1987 <rl1987@users.noreply.github.com>
2022-07-21 14:06:00 +0300

    Extract object details from National Portrait Gallery of London pages

--- museums/spiders/npglondon.py
+++ museums/spiders/npglondon.py
@@ -2,6 +2,8 @@ import scrapy
 
 import string
 
+from museums.items import ObjectItem
+
 class NpglondonSpider(scrapy.Spider):
     name = 'npglondon'
     allowed_domains = ['www.npg.org.uk']
@@ -20,5 +22,36 @@ class NpglondonSpider(scrapy.Spider):
             yield response.follow(l, callback=self.parse_portrait_page)
 
     def parse_portrait_page(self, response):
-        pass
+        item = ObjectItem()
+
+        item['object_number'] = response.xpath('//title/text()').get().split(";")[0]
+        item['institution_name'] = "National Portrait Gallery"
+        item['institution_city'] = "London"
+        item['institution_state'] = 'England'
+        item['institution_country'] = "United Kingdom"
+        item['institution_latitude'] = 51.5094236
+        item['institution_longitude'] = -0.1303103
+        item['title'] = response.xpath('//title/text()').get().split("; ")[-1].replace(" - National Portrait Gallery", "")
+        item['description'] = response.xpath('//meta[@property="og:description"]/@content').get()
+        item['maker_full_name'] = "|".join(response.xpath('//ul[@id="artist-list"]//a/text()').getall())
+        maker_roles = response.xpath('//ul[@id="artist-list"]/li/text()').getall()
+        maker_roles = list(map(lambda mr: mr.split(", ")[-1].split(". ")[0], maker_roles))
+        maker_roles = "|".join(maker_roles)
+        item['maker_role'] = maker_roles
+
+        lines = response.xpath('//div[@class="contentColumn last"]/p[last()]/text()').getall()
+        item['technique'] = ", ".join(lines[1].split(", ")[:-1])
+        item['date_description'] = lines[1].split(", ")[-1]
+        item['dimensions'] = lines[2]
+
+        try:
+            item['acquired_year'] = int(lines[3].split(", ")[-1])
+            item['acquired_from'] = lines[3].split(", ")[0]
+        except:
+            pass
+
+        item['image_url'] = response.xpath('//img[@class="zoom-fallback"]/@src').get()
+        item['source_1'] = response.url
+
+        yield item
 
