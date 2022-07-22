c69b505d97a3a84fbed01e4cc8f5ffd564b6e38d
rl1987 <rl1987@users.noreply.github.com>
2022-07-22 17:35:55 +0300

    Traverse Arts And Culture partner API pages

--- arts_and_culture_mobile/1_scrape_partners.py
+++ arts_and_culture_mobile/1_scrape_partners.py
@@ -2,11 +2,64 @@
 
 import csv
 import json
+from pprint import pprint
 
 import requests
 
+FIELDNAMES = [ "name", "slug", "city", "state", "country", "latitude", "longitude" ]
+
 def main():
-    pass
+    out_f = open("partners.csv", "w", encoding="utf-8")
+
+    csv_writer = csv.DictWriter(out_f, fieldnames=FIELDNAMES, lineterminator="\n")
+    csv_writer.writeheader()
+
+    headers = {
+        'accept': '*/*',
+        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) IOSWebView/9.0.47 ARCapable',
+        'x-same-domain': '1',
+        'accept-language': 'en-GB,en;q=0.9',
+    }
+
+    pt = ''
+
+    while True:
+        params = {
+            's': '30',
+            'pt': pt,
+            'hl': 'en_LT',
+            'rt': 'j',
+        }
+        
+        resp = requests.get('https://artsandculture.google.com/api/objects/partner', params=params, headers=headers)
+        print(resp.url)
+
+        if resp.status_code != 200:
+            print(resp.text)
+            break
+
+        json_str = resp.text
+        json_str = json_str[4:]
+
+        json_arr = json.loads(json_str)
+        
+        try:
+            pt = json_arr[0][0][-1]
+        except:
+            pt = None
+
+        json_arr = json_arr[0][0][2]
+
+        for partner_arr in json_arr:
+            # TODO: convert data to CSV row
+            pass
+        
+        if type(pt) != str:
+            break
+
+        params['pt'] = pt
+
+    out_f.close()
 
 if __name__ == '__main__':
     main()
