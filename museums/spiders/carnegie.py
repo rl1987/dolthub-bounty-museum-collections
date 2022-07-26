ce150ff218e6495d6a328716b3c55aaeb74efc28
rl1987 <rl1987@users.noreply.github.com>
2022-07-26 17:14:18 +0300

    Extract item details from Carnegie Museum of Art API

--- museums/spiders/carnegie.py
+++ museums/spiders/carnegie.py
@@ -3,6 +3,8 @@ import scrapy
 import json
 import logging
 
+from museums.items import ObjectItem
+
 PER_PAGE = 100
 
 class CarnegieSpider(scrapy.Spider):
@@ -75,6 +77,86 @@ class CarnegieSpider(scrapy.Spider):
             from_idx += PER_PAGE
             yield self.create_search_api_request(from_idx)
 
+    def textify_measurements(self, measurements):
+        if measurements is None or len(measurements) == 0:
+            return None
+
+        text = ""
+        
+        for submeasurement in measurements:
+            text += submeasurement.get('type', "") + ': '
+            text += submeasurement.get('fraction', dict()).get('height', "") + ' x ' + submeasurement.get('fraction', dict()).get('width', "")
+            if submeasurement.get('fraction', dict()).get('depth') is not None:
+                text += ' x ' + submeasurement.get('fraction').get('depth')
+
+            text += ' in ('
+            text += submeasurement.get('decimal', dict()).get('width', "") + ' x ' + submeasurement.get('decimal', dict()).get('height', "")
+            if submeasurement.get('decimal', dict()).get('depth') is not None:
+                text += ' x ' + submeasurement.get('decimal').get('depth')
+            text += ' cm)\n'
+
+        return text
+
     def parse_source_api_response(self, response):
-        pass
+        json_str = response.text
+        json_dict = json.loads(json_str)
+
+        item = ObjectItem()
+
+        item['object_number'] = json_dict.get('id')
+        item['institution_name'] = 'Carnegie Museum of Art'
+        item['institution_city'] = 'Pittsburgh'
+        item['institution_state'] = 'PA'
+        item['institution_country'] = 'United States'
+        item['institution_latitude'] = 40.4437052
+        item['institution_longitude'] = -79.9511614
+        item['department'] = json_dict.get('department')
+        item['category'] = json_dict.get('type')
+        item['title'] = json_dict.get("title")
+        # XXX: description
+        item['current_location'] = json_dict.get("current_location")
+        item['dimensions'] = self.textify_measurements(json_dict.get('measurements'))
+        # XXX: inscription, provenance
+        item['materials'] = json_dict.get('medium')
+        item['technique'] = json_dict.get("medium_description")
+        item['from_location'] = json_dict.get('creation_address')
+        item['date_description'] = json_dict.get("creation_date")[0]
+
+        try:
+            item['year_start'] = int(json_dict.get('creation_date')[0])
+            item['year_end'] = int(json_dict.get('creation_date')[-1])
+        except:
+            pass
+
+        makers = json_dict.get("creators", [])
+        if makers is not None and len(makers) > 0:
+            maker_names = list(map(lambda m: m.get("label"), makers))
+            maker_birth_years = list(map(lambda m: str(m.get("birth", "")), makers))
+            maker_death_years = list( map(lambda m: str(m.get("death", "")), makers ))
+            maker_genders = list( map(lambda m: str(m.get("gender")), makers) )
+
+            item['maker_full_name'] = "|".join(maker_names)
+            item['maker_birth_year'] = "|".join(maker_birth_years).replace("None", "")
+            item['maker_death_year'] = "|".join(maker_death_years).replace("None", "")
+            item['maker_gender'] = "|".join(maker_genders).replace("None", "")
+    
+        try:
+            item['acquired_year'] = json_dict.get("acquisition_date","").split("-")[0]
+        except:
+            pass
+
+        item['acquired_from'] = json_dict.get("acquisition_method")
+        item['accession_number'] = json_dict.get("accession_number")
+        item['credit_line'] = json_dict.get("credit_line")
+
+        if len(json_dict.get('images', [])) > 0:
+            filename = json_dict.get('images')[0].get('filename')
+            irn = json_dict.get('images')[0].get('irn')
+            item['image_url'] = "https://cmoa-collection-images.s3.amazonaws.com/{}/{}/{}".format(
+                item['category'], irn, filename)
+        
+        item['source_1'] = 'https://collection.cmoa.org/objects/' + item['object_number'].replace('cmoa:objects/', '')
+        item['source_2'] = response.url
+
+        yield item
 
