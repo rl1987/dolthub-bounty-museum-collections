1a8284c513afe033a9833effb1e9f5a3d9f93866
rl1987 <rl1987@users.noreply.github.com>
2022-07-27 11:38:39 +0300

    Try to simplify BranlySpider to simply enumerate all possible object indices

--- museums/spiders/branly.py
+++ museums/spiders/branly.py
@@ -1,43 +1,15 @@
 import scrapy
-from scrapy.selector import Selector
-
-import json
-from urllib.parse import urlparse, parse_qsl, urlencode
 
 from museums.items import ObjectItem
 
 class BranlySpider(scrapy.Spider):
     name = 'branly'
     allowed_domains = ['www.quaibranly.fr']
-    start_urls = ['https://www.quaibranly.fr/fr/explorer-les-collections/base/Work/action/list/?format=json&search=true&category=oeuvres&page=1&mode=thumb&orderby=null&order=desc&category=oeuvres&tx_mqbcollection_explorer%5Bquery%5D%5Btype%5D=&tx_mqbcollection_explorer%5Bquery%5D%5Bclassification%5D=&tx_mqbcollection_explorer%5Bquery%5D%5Bexemplaire%5D=']
+    start_urls = ['https://www.quaibranly.fr/en/explore-collections/base/Work/action/show/notice/' + str(i) for i in range(1, 10000000)]
 
     def start_requests(self):
-        yield scrapy.Request(self.start_urls[0], callback=self.parse_search_page)
-
-    def parse_search_page(self, response):
-        json_str = response.text
-        json_dict = json.loads(json_str)
-        html_str = json_dict.get("html")
-
-        sel = Selector(text=html_str)
-
-        for link in sel.xpath('//figure/a/@href').getall():
-            yield response.follow(link, callback=self.parse_object_page)
-
-        total_pages = json_dict.get("nbPages")
-
-        o = urlparse(response.url)
-        old_params = dict(parse_qsl(o.query))
-
-        params = dict(old_params)
-        page = int(params['page'])
-
-        if page <= total_pages:
-            page += 1
-            params['page'] = page
-
-            next_page_url = 'https://www.quaibranly.fr/fr/explorer-les-collections/base/Work/action/list/?' + urlencode(params)
-            yield scrapy.Request(next_page_url, callback=self.parse_search_page)
+        for start_url in self.start_urls:
+            yield scrapy.Request(start_url, callback=self.parse_object_page)
 
     def parse_object_page(self, response):
         item = ObjectItem()
