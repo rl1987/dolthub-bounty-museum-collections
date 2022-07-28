f4591c875fd37f8ce44b99842d914b153e7c77fc
rl1987 <rl1987@users.noreply.github.com>
2022-07-28 16:30:44 +0300

    Reuse parse_object_page() from EhiveSpider in NzmuseumsSpider

--- museums/spiders/nzmuseums.py
+++ museums/spiders/nzmuseums.py
@@ -1,5 +1,6 @@
 import scrapy
 
+from museums.items import ObjectItem
 
 class NzmuseumsSpider(scrapy.Spider):
     name = 'nzmuseums'
@@ -18,4 +19,84 @@ class NzmuseumsSpider(scrapy.Spider):
             yield response.follow(next_page_link, callback=self.parse_search_page)
 
     def parse_object_page(self, response):
-        pass
+        self.logger.debug(response.meta)
+
+        item = ObjectItem()
+
+        item["object_number"] = (
+            response.xpath('//p[contains(@class, "object_number")]/text()')
+            .get("")
+            .strip()
+        )
+
+        item["institution_name"] = response.meta.get("institution_name")
+        item["institution_latitude"] = response.meta.get("institution_latitude")
+        item["institution_longitude"] = response.meta.get("institution_longitude")
+
+        item["category"] = (
+            response.xpath('//p[contains(@class, "object_type")]/a/text()')
+            .get("")
+            .strip()
+        )
+        item["department"] = (
+            response.xpath('//p[contains(@class, "named_collection")]/text()')
+            .get("")
+            .strip()
+        )
+        item["title"] = (
+            response.xpath('//p[contains(@class, "name")]/text()').get("").strip()
+        )
+        item["description"] = response.xpath(
+            '//p[contains(@class, "web_public_description")]/text()'
+        ).getall()
+        item["description"] = list(map(lambda d: d.strip(), item["description"]))
+        item["description"] = " ".join(item["description"])
+
+        item["dimensions"] = (
+            response.xpath('//p[contains(@class, "measurement_description")]/text()')
+            .get("")
+            .strip()
+        )
+        item["inscription"] = (
+            response.xpath('//p[contains(@class, "inscription")]/text()')
+            .get("")
+            .strip()
+        )
+        item["materials"] = response.xpath(
+            '//p[contains(@class, "medium_description")]/text()'
+        ).getall()
+        item["materials"] = list(map(lambda m: m.strip(), item["materials"]))
+        item["materials"] = " ".join(item["materials"])
+        item["from_location"] = (
+            response.xpath('//p[contains(@class, "place_made")]/a/text()')
+            .get("")
+            .strip()
+        )
+        item["date_description"] = (
+            response.xpath('//p[contains(@class, "date_made")]/text()').get("").strip()
+        )
+        item["maker_full_name"] = response.xpath(
+            '//p[contains(@class, "primary_creator_maker")]/a/text()'
+        ).getall()
+        # Fuck this is getting repetitive...
+        item["maker_full_name"] = list(
+            map(lambda m: m.strip(), item["maker_full_name"])
+        )
+        item["maker_full_name"] = "|".join(item["maker_full_name"])
+        item["maker_role"] = response.xpath(
+            '//p[contains(@class, "primary_creator_maker_role")]/text()'
+        ).getall()
+        item["maker_role"] = list(map(lambda m: m.strip(), item["maker_role"]))
+        item["maker_role"] = "|".join(item["maker_role"])
+
+        item["credit_line"] = (
+            response.xpath('//p[contains(@class, "credit_line")]/text()')
+            .get("")
+            .strip()
+        )
+        item["image_url"] = response.xpath(
+            '//a[./img[@class="eh-low-res-image"]]/@href'
+        ).get()
+        item["source_1"] = response.url
+
+        yield item
