b2f8ae914a0169d4e9af3d4398e3ea3b5c6f1e3b
rl1987 <rl1987@users.noreply.github.com>
2022-07-28 16:18:29 +0300

    Crawl the search on NZ Museums portal

--- museums/spiders/nzmuseums.py
+++ museums/spiders/nzmuseums.py
@@ -4,7 +4,18 @@ import scrapy
 class NzmuseumsSpider(scrapy.Spider):
     name = 'nzmuseums'
     allowed_domains = ['www.nzmuseums.co.nz']
-    start_urls = ['http://www.nzmuseums.co.nz/']
+    start_urls = ['https://www.nzmuseums.co.nz/objects?view=lightbox']
 
-    def parse(self, response):
+    def start_requests(self):
+        yield scrapy.Request(self.start_urls[0], callback=self.parse_search_page)
+
+    def parse_search_page(self, response):
+        for artwork_link in response.xpath('//div[@class="lightbox-object-desc "]/a/@href'):
+            yield response.follow(artwork_link, callback=self.parse_object_page)
+
+        next_page_link = response.xpath('//a[@aria-label="Next"]/@href').get()
+        if next_page_link is not None:
+            yield response.follow(next_page_link, callback=self.parse_search_page)
+
+    def parse_object_page(self, response):
         pass
