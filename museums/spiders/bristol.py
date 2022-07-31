2bb0e3fd03ca3349b551020ecb2653726beb4ef9
rl1987 <rl1987@users.noreply.github.com>
2022-07-31 15:05:51 +0300

    Crawl the pages on Bristol museum portal

--- museums/spiders/bristol.py
+++ museums/spiders/bristol.py
@@ -4,7 +4,18 @@ import scrapy
 class BristolSpider(scrapy.Spider):
     name = 'bristol'
     allowed_domains = ['museums.bristol.gov.uk']
-    start_urls = ['http://museums.bristol.gov.uk/']
+    start_urls = ['https://museums.bristol.gov.uk/list.php']
 
-    def parse(self, response):
+    def start_requests(self):
+        yield scrapy.Request(self.start_urls[0], callback=self.parse_object_list)
+
+    def parse_object_list(self, response):
+        for l in response.xpath('//div[@class="media"]//a/@href').getall():
+            yield response.follow(l, callback=self.parse_object_page)
+
+        next_page_link = response.xpath('//li[@class="next"]/a/@href').get()
+        if next_page_link is not None:
+            yield response.follow(next_page_link, callback=self.parse_object_list)
+
+    def parse_object_page(self, response):
         pass
