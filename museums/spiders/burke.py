2f676c25408a851ac3f2eec5e30e92d69b35cc36
rl1987 <rl1987@users.noreply.github.com>
2022-08-05 19:13:50 +0300

    Try traversing art collection pages on Burke Museum site

--- museums/spiders/burke.py
+++ museums/spiders/burke.py
@@ -4,7 +4,17 @@ import scrapy
 class BurkeSpider(scrapy.Spider):
     name = 'burke'
     allowed_domains = ['www.burkemuseum.org']
-    start_urls = ['http://www.burkemuseum.org/']
+    start_urls = ['https://www.burkemuseum.org/collections-and-research/culture/contemporary-culture/database/browse.php']
 
-    def parse(self, response):
+    def start_requests(self):
+        yield scrapy.Request(self.start_urls[0], callback=self.parse_art_collection_page)
+
+    def parse_art_collection_page(self, response):
+        for l in response.xpath('//td[@class="collectionsText"]/a/@href').getall():
+            yield response.follow(l, callback=self.parse_art_collection_page)
+
+        for l in response.xpath('//td[@class="resultsData"]/a/@href').getall():
+            yield response.follow(l, callback=self.parse_artwork_page)
+
+    def parse_artwork_page(self, response):
         pass
