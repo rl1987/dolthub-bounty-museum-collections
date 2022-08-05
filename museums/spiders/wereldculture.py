0ad3c1876804007e6549ad092fb3b194170066e9
rl1987 <rl1987@users.noreply.github.com>
2022-08-05 16:01:48 +0300

    Improve object page scraping in WereldcultureSpider

--- museums/spiders/wereldculture.py
+++ museums/spiders/wereldculture.py
@@ -957,26 +957,27 @@ class WereldcultureSpider(scrapy.Spider):
         item["institution_country"] = "Netherlands"
         item["institution_latitude"] = 52.3626561
         item["institution_longitude"] = 4.9211787
-        # XXX: category, department
-        item["title"] = sel.xpath('//h3[@data-drag="true"]/text()').get()
-        # XXX: current_location, dimensions, inscription
-        item["description"] = " ".join(
-            sel.xpath('//div[./button[@data-toggle="modal"]]/text()').getall()
-        ).strip()
-        item["provenance"] = "|".join(
-            sel.xpath(
-                '//div[./strong[text()="Origin : " or text()="Herkomst : "]]/span/text()'
-            ).getall()
-        )
-        item["materials"] = sel.xpath(
-            '//strong[text()="Medium : " or text()="Materiaal : "]/following-sibling::text()'
+        item["title"] = "".join(sel.xpath('//div[@ng-if="cc.language==\'en\'"]/h3[@data-drag="true"]//text()').getall()).strip()
+        
+        lines = sel.xpath('//div[@class="panel-body"]/div[@ng-if="cc.language==\'en\'"]//text()').getall()
+
+        item["description"] = " ".join(lines).strip().split("{{userdata.baskets[$basketIndex].description}}")[0]
+
+        for i in range(0, len(lines)-1):
+            l = lines[i]
+            if l == "Origin : " or l == "Herkomst : ":
+                item['provenance'] = lines[i+1]
+            elif l == "Medium : " or l == "Materiaal : ":
+                item['materials'] = lines[i+1]
+            elif l == "Culture : " or l == "Cultuur : ":
+                item['culture'] = lines[i+1]
+            elif l == "Creditline : ":
+                item['credit_line'] = lines[i+1]
+
+        item["source_1"] = sel.xpath(
+            '//div[./b[text()="Permanent link to this object : "]]/text()'
         ).get()
-        item["culture"] = sel.xpath(
-            '//strong[text()="Culture : " or text()="Cultuur : "]/following-sibling::text()'
-        ).get()
-        item["credit_line"] = sel.xpath(
-            '//strong[text()="Creditline : "]/following-sibling::text()'
-        ).get()
+
         item["image_url"] = sel.xpath(
             '//*[@data-drag="true"]/@data-draggingimage'
         ).get()
@@ -984,8 +985,5 @@ class WereldcultureSpider(scrapy.Spider):
             item["image_url"] = urljoin(
                 response.url, "/" + item["image_url"].split("&width")[0]
             )
-        item["source_1"] = sel.xpath(
-            '//div[./b[text()="Permanent link to this object : "]]/text()'
-        ).get()
 
         yield item
