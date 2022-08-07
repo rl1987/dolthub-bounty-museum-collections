f93f0e39225e0881294c59df0fa299449dec2f9e
rl1987 <rl1987@users.noreply.github.com>
2022-08-07 14:38:46 +0300

    Crawl the pages on museums.eu

--- museums/spiders/museumseu.py
+++ museums/spiders/museumseu.py
@@ -4,7 +4,24 @@ import scrapy
 class MuseumseuSpider(scrapy.Spider):
     name = 'museumseu'
     allowed_domains = ['museums.eu']
-    start_urls = ['http://museums.eu/']
+    start_urls = ['http://museums.eu/search/index?documenttype=collection&page=1']
 
-    def parse(self, response):
+    def start_requests(self):
+        yield scrapy.Request(self.start_urls[0], callback=self.parse_collection_list)
+
+    def parse_collection_list(self, response):
+        for l in response.xpath('//h4[@class="media-heading"]/a/@href').getall():
+            yield response.follow(l, callback=self.parse_object_list)
+
+        for l in response.xpath('//ul[@class="pagination"]/li/a/@href'):
+            yield response.follow(l, callback=self.parse_collection_list)
+
+    def parse_object_list(self, response):
+        for l in response.xpath('//div[@class="artwork"]/a/@href').getall():
+            yield response.follow(l, callback=self.parse_object_page)
+
+        for l in response.xpath('//ul[@id="collection-pagination"]/li/a/@href').getall():
+            yield response.follow(l, callback=self.parse_object_list)
+
+    def parse_object_page(self, response):
         pass
